8a4c68bbf2e583a620a623f2c1c9862c371c8131
Zr-udn <badboyav555@gmail.com>
2026-09-07 21:38:36 +0530

    Change Supabase URL and key quotes to single quotes
    Updated Supabase URL and key string quotes from double to single.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ⚠️ YAHAN APNI SUPABASE DETAILS DAAL (SABSE ZAROORI STEP)
-const SUPABASE_URL = "https://hafkxgipiqjmjhevjlsc.supabase.co";
-const SUPABASE_KEY = "sb_publishable_cbG1R6WokyUczPW4OWLHww_hqlM0JGg";
+const SUPABASE_URL = 'https://hafkxgipiqjmjhevjlsc.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_cbG1R6WokyUczPW4OWLHww_hqlM0JGg';
 
 let supabase;
 try {
