d0749e1efa7a974a4021ab8af609194e1436e027
Zr-udn <badboyav555@gmail.com>
2026-09-07 17:27:22 +0530

    Update Supabase credentials in script.js
    Updated Supabase URL and key in script.js.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ⚠️ YAHAN APNI SUPABASE DETAILS DAAL (SABSE ZAROORI STEP)
-const SUPABASE_URL = "TERI_SUPABASE_URL_YAHAN";
-const SUPABASE_KEY = "TERI_SUPABASE_ANON_KEY_YAHAN";
+const SUPABASE_URL = "https://hafkxgipiqjmjhevjlsc.supabase.co";
+const SUPABASE_KEY = "sb_publishable_cbG1R6WokyUczPW4OWLHww_hqlM0JGg";
 
 let supabase;
 try {
